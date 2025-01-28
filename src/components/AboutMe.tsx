fbff0294a766b5864c3885c3f172aba06dfec6f7
Bianca Rossetti <biancaros05@icloud.com>
2025-01-27 21:11:07 -0500

    Fix spelling mistake

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,7 +6,12 @@ function AboutMe() {
   return (
     <div className="container">
       <h2>About me</h2> 
-      <p>Hey there :) My name is Bianca Rossetti and I'm a Computer Science student at Dawson College. Throughout my time there, I learned a wide array of different technologies, soem of my favourites being Full Stack Development and Programming various apps and games! Though I do work with computers a lot, in my free time, I also love to read and meet with my friends! Some day, i hope my work can have an impact on at least one person for the better, because that would mean, to me, that I am fulfulling my duty as a Computer Scientist to make people's lives easier through automation and programmation! And someday, I hope my work manages to travel to space and back :)</p>
+      <p>Hey there! My name is Bianca Rossetti and I'm a <b>Computer Science student</b> at Dawson College. 
+        Throughout my time there, I learned a wide array of different technologies, some of my favourites being <b>Full Stack Development and Programming</b> various apps and games! 
+        Though I do work with computers a lot, in my free time, I also love to <b>read and meet with my friends</b>! 
+        Some day, i hope <b>my work can have an impact</b> on at least one person for the better, because that would mean, 
+        to me, that I am <b>fulfulling my duty as a Computer Scientist</b> to make people's lives easier through automation 
+        and programmation! And someday, I hope my work manages to travel to space and back :)</p>
     </div>
   );
 }
